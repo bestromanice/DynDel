786d87be396dafd17c38cbd22f208a79cb7cae8c
Roman Kaluga <kaluga.roma25@gmail.com>
2023-06-14 17:50:35 +0700

    added second activity

--- app/src/main/java/com/example/dyndel/MainActivity.java
+++ app/src/main/java/com/example/dyndel/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.dyndel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button downloadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        downloadButton = findViewById(R.id.download_module_button);
+
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
